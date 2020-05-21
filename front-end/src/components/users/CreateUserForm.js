a5b1055ffe138f62d8250ede97b20a69237bc1f3
unknown <miloszz87@gmail.com>
2020-05-21 00:10:40 -0400

    Added more styling to create account form

--- front-end/src/components/users/CreateUserForm.js
+++ front-end/src/components/users/CreateUserForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as yup from "yup";
 import axios from "axios";
 import styled from "styled-components";
-import ".../createuser.css";
+import "./createuser.css";
 /////////////////////////
 ///       FORM        ///
 /////////////////////////
@@ -10,7 +10,10 @@ import ".../createuser.css";
 const CreateUserForm = () => {
   return (
     <div className="create-account-form">
-      <CreateAccountHeading>Create Account</CreateAccountHeading>
+      <CreateAccountHeading>We're Here to help.</CreateAccountHeading>
+      <CreateAccountSubHeading>
+        Create a help ticket and we'll connect you with a Lambda School Helper.
+      </CreateAccountSubHeading>
       <form>
         <label htmlFor="name">
           {/* NAME */}
@@ -37,12 +40,13 @@ const CreateUserForm = () => {
           />
         </label>
         <label htmlFor="role" className="role">
-          Pick Your Role:
+          My Role:
+          <input type="checkbox" id="student" name="student" value="student" />
+          <label htmlFor="role">Student</label>
+          <input type="checkbox" id="helper" name="helper" value="helper" />
+          <label htmlFor="role">Helper</label>
         </label>
-        <input type="checkbox" id="student" name="student" value="student" />
-        <label htmlFor="role">Student</label>
-        <input type="checkbox" id="helper" name="helper" value="helper" />
-        <label htmlFor="role">Helper</label>
+        <button type="submit">Create Account</button>
       </form>
     </div>
   );
@@ -59,6 +63,18 @@ const CreateAccountHeading = styled.span`
   line-height: 1.2;
   text-align: center;
   display: block;
+  padding-bottom: 25px;
+`;
+
+const CreateAccountSubHeading = styled.span`
+  color: #333333;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 1.2;
+  text-align: center;
+  display: block;
+  width: auto;
+  margin: 0 5%;
   padding-bottom: 25px;
 `;
 
