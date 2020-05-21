a304cf51ebdeec58d50175900fc777dae2c323eb
unknown <miloszz87@gmail.com>
2020-05-21 11:30:48 -0400

    Finished JSX and Styling for Create User Account Form

--- front-end/src/components/users/CreateUserForm.js
+++ front-end/src/components/users/CreateUserForm.js
@@ -3,6 +3,7 @@ import * as yup from "yup";
 import axios from "axios";
 import styled from "styled-components";
 import "./createuser.css";
+
 /////////////////////////
 ///       FORM        ///
 /////////////////////////
@@ -10,44 +11,47 @@ import "./createuser.css";
 const CreateUserForm = () => {
   return (
     <div className="create-account-form">
-      <CreateAccountHeading>We're Here to help.</CreateAccountHeading>
-      <CreateAccountSubHeading>
-        Create a help ticket and we'll connect you with a Lambda School Helper.
-      </CreateAccountSubHeading>
-      <form>
-        <label htmlFor="name">
-          {/* NAME */}
-          <input className="input" id="name" type="text" placeholder="Name" />
-        </label>
-        <label htmlFor="email">
-          {/* EMAIL */}
-          <input
-            className="input"
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-        </label>
-        <label htmlFor="password">
-          {/* PASSWORD */}
-          <input
-            className="input"
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-        </label>
-        <label htmlFor="role" className="role">
-          My Role:
-          <input type="checkbox" id="student" name="student" value="student" />
-          <label htmlFor="role">Student</label>
-          <input type="checkbox" id="helper" name="helper" value="helper" />
-          <label htmlFor="role">Helper</label>
-        </label>
-        <button type="submit">Create Account</button>
-      </form>
+      <BackgroundWrap>
+        <CreateAccountHeading>We're Here to help.</CreateAccountHeading>
+        <CreateAccountSubHeading>
+          Create a help ticket and we'll connect you with a Lambda School
+          Helper.
+        </CreateAccountSubHeading>
+        <form>
+          <label htmlFor="name">
+            {/* NAME */}
+            <input className="input" id="name" type="text" placeholder="Name" />
+          </label>
+          <label htmlFor="email">
+            {/* EMAIL */}
+            <input
+              className="input"
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </label>
+          <label htmlFor="password">
+            {/* PASSWORD */}
+            <input
+              className="input"
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </label>
+          <label htmlFor="role" className="role">
+            My Role:
+            <input type="checkbox" id="student" name="role" value="student" />
+            <label htmlFor="role">Student</label>
+            <input type="checkbox" id="helper" name="role" value="helper" />
+            <label htmlFor="role">Helper</label>
+          </label>
+          <button type="submit">Create Account</button>
+        </form>
+      </BackgroundWrap>
     </div>
   );
 };
@@ -64,6 +68,7 @@ const CreateAccountHeading = styled.span`
   text-align: center;
   display: block;
   padding-bottom: 25px;
+  margin: 0 0 0 5%;
 `;
 
 const CreateAccountSubHeading = styled.span`
@@ -73,9 +78,21 @@ const CreateAccountSubHeading = styled.span`
   line-height: 1.2;
   text-align: center;
   display: block;
-  width: auto;
-  margin: 0 5%;
+  width: 330px;
+  margin: 0 35%;
   padding-bottom: 25px;
+`;
+
+const BackgroundWrap = styled.div`
+  background: #fff;
+  border-radius: 10px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -300px;
+  margin-left: -600px;
+  width: 960px;
+  padding: 30px 130px 130px 95px;
 `;
 
 export default CreateUserForm;
