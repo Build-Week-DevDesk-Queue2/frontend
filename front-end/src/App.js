7da7299859e317daef507eb4d203c243ca8622a8
Darrell-Conklin <dmconklin84@gmail.com>
2020-05-24 18:10:48 -0500

    after pulling from master commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import CreateUserForm from "./components/users/CreateUserForm";
 import LoginForm from "./components/users/LoginForm";
 import StudentSuccessPage from "./components/pages/StudentSuccessPage";
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => localStorage.getItem('token') ? (
+    <Component {...props} />
+  ) : (
+    <Redirect to="/" />
+  )} />
+)
 
 function App() {
   return (
     <div className="App">
-      <StudentSuccessPage />
+      <Route exact path="/" component={LoginForm} />
+      <Route path="/sign-up" component={CreateUserForm} />
+      <Route path="/student-success" component={StudentSuccessPage} />
     </div>
   );
 }
